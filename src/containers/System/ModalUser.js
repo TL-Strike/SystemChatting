65f19e5239ac9e7d2dca2dcf2547ce7f51dfb27c
Strike Nguyen <thailinh.itk19@gmail.com>
2025-04-10 04:39:28 +0700

    React DELETE

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { emitter } from '../../utils/emitter';
 
 class ModalUser extends Component {
 
@@ -15,9 +16,28 @@ class ModalUser extends Component {
             lastName: '',
             address: ''
         }
+
+        this.listenToEmitter();
+    }
+
+    listenToEmitter() {
+        // emitter.on('EVENT_CLEAR_MODAL_DATA', data => {
+        //     console.log('listen emitter from parent: ', data)
+        // })
+        emitter.on('EVENT_CLEAR_MODAL_DATA', () => {
+            //reset state
+            this.setState({
+                email: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+                address: ''
+            })
+        })
     }
 
     componentDidMount() {
+        console.log('mounting modal')
     }
 
     toggle = () => {
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import {getAllUsers, createNewUserService} from '../../services/userService';
+import {getAllUsers, createNewUserService, deleteUserService} from '../../services/userService';
 import ModalUser from './ModalUser';
+import {emitter} from '../../utils/emitter'
 class UserManage extends Component {
     constructor(props) {
         super(props);
@@ -56,13 +57,30 @@ class UserManage extends Component {
                 this.setState({
                     isOpenModalUser: false
                 })
+                // emitter.emit('EVENT_CLEAR_MODAL_DATA', {'id': 'your id'}) //fire event
+                emitter.emit('EVENT_CLEAR_MODAL_DATA') //fire event
             }
-            console.log("response create user: ", response)
+            // console.log("response create user: ", response)
         } catch (e) {
             console.log(e)
         }
         
         // console.log('data from child ', data);
+    }
+
+    handleDeleteUser = async (user) => {
+        // console.log('click delete: ', user)
+        try {
+            let response = await deleteUserService(user.id);
+            if(response && response.errCode === 0) {
+                await this.getAllUsersFromReact();
+            }
+            else {
+                alert(response.errMessage);
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     render() {
@@ -103,7 +121,7 @@ class UserManage extends Component {
                                             <td>{item.address}</td>
                                             <td>
                                                 <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
-                                                <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                                                <button className="btn-delete" onClick={()=>this.handleDeleteUser(item)}><i className="fas fa-trash"></i></button>
                                             </td>
                                         </tr>
                                     )
